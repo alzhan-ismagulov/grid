7074eee961fd47c191897dc28cef8bc9c59557c8
alzhan-ismagulov <aismagulov@gmail.com>
2020-08-06 21:55:07 +0200

    20. Анимация линий блока прогресса

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-// $(document).ready(function() {
-//     $('.works__btns button, .works__btns a').on('focus', function (e){
-//        $(this).parents('.works__element').addClass('.works__element--active');
-//     });
-//     $('.works__btns button, .works__btns a').on('blur', function (e){
-//         $(this).parents('.works__element').removeClass('.works__element--active');
-//     });
-// });
-
 $(document).ready(function () {
 
     $('.works__btns button, .works__btns a').on('focus', function (e) {
@@ -17,4 +8,27 @@ $(document).ready(function () {
         $(this).parents('.works__element').removeClass('works__element--active');
     });
 
+    function moveProgressBar(node, nodeLine, tooltip, animationLength = 1500) {
+        const progressElement = $(node);
+        progressElement.each(function (value, item) {
+            $(item).find(nodeLine).animate({
+                width: item.dataset.progressPercent+'%'
+            }, animationLength);
+            $(item).find(tooltip).show(animationLength);
+        });
+    }
+
+    let animate = true;
+
+
+    $(window).scroll(function () {
+
+        if($('.skills').offset().top <= $(window).scrollTop() + 150) {
+            if(animate) {
+                moveProgressBar('.progress__element', '.progress__line', '.progress__tooltip');
+            }
+            animate = false;
+        }
+
+    });
 });
